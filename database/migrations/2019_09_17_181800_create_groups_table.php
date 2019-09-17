ae793bf30820c6e97fa8519f2f8f0a4e6d8c0a07
IsaackHazaell <isaachvc@hotmail.com>
2019-09-17 13:50:35 -0500

    Se comenzo con el modulo grupo

---
+++ database/migrations/2019_09_17_181800_create_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->biginteger('semester');
+            $table->string('letter');
+            $table->integer('turn')->unsigned();
+            $table->foreign('subject_id')
+                   ->references('id')->on('subjects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
