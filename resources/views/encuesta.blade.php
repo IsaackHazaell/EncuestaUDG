378dd9a1269e5104ad27f79c411528557eb5913c
IsaackHazaell <isaachvc@hotmail.com>
2019-09-18 18:43:34 -0500

    Se comenzo a trabajar en el diseño de encuestas

--- resources/views/encuesta.blade.php
+++ resources/views/encuesta.blade.php
@@ -3,11 +3,36 @@
   <head>
     <meta charset="utf-8">
     <title>Encuesta</title>
+
+    <!-- Estilo de la página -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+    <!-- Font Awesome CDN -->
+    <script src="https://kit.fontawesome.com/23412c6a8d.js"></script>
+
   </head>
   <body>
-    <h1>Encuesta</h1>
-    <img src="image\sistemadiagnostico.jpg" alt="">
-    <!-- Aún no se ve la imagen -->
-  </body>
 
+    <div class="container">
+      <div class="panel">
+        <div class="row">
+          <div class="col liquid">
+            <h4>Encuestas Prepa 13.</h4>
+
+            <!-- Carrusel -->
+            <div class="owl-carousel owl-theme">
+              <img src="{{asset('storage/descarga.svg')}}" alt="" class="login_img">
+              <img src="{{asset('storage/descarga2.svg')}}" alt="" class="login_img">
+              <img src="{{asset('storage/descarga3.svg')}}" alt="" class="login_img">
+            </div>
+            <!-- /Carrusel -->
+          </div>
+          <div class="col">
+          
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
 </html>
