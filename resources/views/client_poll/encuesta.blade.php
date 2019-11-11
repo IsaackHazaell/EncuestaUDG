78496430dc03f370fb80750381fb8771c260c7e4
IsaackHazaell <isaachvc@hotmail.com>
2019-11-11 13:21:17 -0600

    Se aplico estilos a encuestas de parte del cliente

--- resources/views/client_poll/encuesta.blade.php
+++ resources/views/client_poll/encuesta.blade.php
@@ -15,25 +15,32 @@
         <img src="{{ asset("img/logoudg.png") }}" style="width:65px;" alt="">
       </div>
 
-      <div>
-        <h1>Encuestas</h1>
+      <div class="header-container">
+        <div class="header">
+          <h1>Encuesta</h1>
+        </div>
       </div>
 
-      <section style="display: block;" class="body current" id="wizard-p-4" role="tabpanel" aria-labelledby="wizard-h-4" aria-hidden="false">
-        <form action=" {{route('poll.show_polls')}} " method="POST">
-          @csrf
-              <div class="form-group col-md-8">
-                <select class="form-control" name="group" id="group" onchange="show();">
-                    <option hidden disabled selected value> -- select an option -- </option>
-                    @foreach ($groups as $group)
-                      <option value="{{$group->id}}">{{$group->semester}}-{{$group->letter}} - {{$group->turn}}</option>
-                    @endforeach
-                </select>
-              </div>
-
-              <button id="send" class="btn btn-success" type="submit">Aceptar</button>
-          </form>
-      </section>
+      <div class="poll-container">
+        <section style="display: block;" class="body current" id="wizard-p-4" role="tabpanel" aria-labelledby="wizard-h-4" aria-hidden="false">
+          <form action=" {{route('poll.show_polls')}} " method="POST">
+            @csrf
+                <div class="form-group">
+                  <div class="poll-container select">
+                    <select class="form-control" name="group" id="group" onchange="show();">
+                        <option hidden disabled selected value> Selecciona tu grupo </option>
+                        @foreach ($groups as $group)
+                          <option value="{{$group->id}}">{{$group->semester}}-{{$group->letter}} - {{$group->turn}}</option>
+                        @endforeach
+                    </select>
+                  </div>
+                  <div class="poll-container button">
+                    <button id="send" class="btn btn-success" type="submit">Aceptar</button>
+                  </div>
+                </div>
+            </form>
+        </section>
+      </div>
 
     </body>            
      
@@ -42,6 +49,7 @@
     <link rel="stylesheet" href="{{asset('adminlte/bower_components/Ionicons/css/ionicons.min.css')}}">
     <link rel="stylesheet" href="{{asset('adminlte/css/AdminLTE.min.css')}}">
     <link rel="stylesheet" href="{{asset('adminlte/css/skins/skin-blue.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/client_poll.css')}}">
     {{-- <script src="{{asset('adminlte/bootstrap/dist/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('adminlte/js/adminlte.min.js')}}"></script> --}}
     @include('client_poll.partials.script')
